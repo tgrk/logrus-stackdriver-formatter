b916ae31cee551d8633587c0c4db38b449f35ebd
Paula Babbicola <paula.babbicola@morressier.com>
2019-04-17 12:03:08 +0200

    Add go mod and fix logger.go issues

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/pbabbicola/logrus-stackdriver-formatter
+
+go 1.12
+
+require (
+	github.com/go-kit/kit v0.8.0
+	github.com/go-logfmt/logfmt v0.4.0 // indirect
+	github.com/go-stack/stack v1.8.0
+	github.com/google/go-cmp v0.2.0
+	github.com/jinzhu/copier v0.0.0-20180308034124-7e38e58719c3
+	github.com/sirupsen/logrus v1.4.1
+)
--- logger.go
+++ logger.go
@@ -3,9 +3,9 @@ package stackdriver
 import (
 	"io"
 
-	"github.com/Sirupsen/logrus"
 	"github.com/go-kit/kit/log"
 	"github.com/jinzhu/copier"
+	"github.com/sirupsen/logrus"
 )
 
 // Logger is a gokit-compatible wrapper for logrus.Logger
@@ -27,8 +27,8 @@ func NewStackdriverLogger(w io.Writer, opts ...Option) *Logger {
 //
 // The returned Logger replaces all value elements (odd indexes) containing a
 // Valuer with their generated value for each call to its Log method.
-func With(logger Logger, vals ...interface{}) *Logger {
-	if len(keyvals) == 0 {
+func With(logger *Logger, vals ...interface{}) *Logger {
+	if len(vals) == 0 {
 		return logger
 	}
 	kvs := make([]interface{}, len(vals))
@@ -40,7 +40,7 @@ func With(logger Logger, vals ...interface{}) *Logger {
 		kvs = append(vals, log.ErrMissingValue)
 	}
 	for i := 0; i < len(kvs); i = i + 2 {
-		if k, ok := kvs.(string); ok {
+		if k, ok := kvs[i].(string); ok {
 			logger.Logger.WithField(k, kvs[i+1])
 		}
 	}
@@ -50,16 +50,17 @@ func With(logger Logger, vals ...interface{}) *Logger {
 // Log creates a log event from keyvals, a variadic sequence of alternating
 // keys and values.
 func (l Logger) Log(keyvals ...interface{}) error {
-	kvs := make([]interface{}, len(vals))
-	err := copier.Copy(kvs, vals)
+	kvs := make([]interface{}, len(keyvals))
+	err := copier.Copy(kvs, keyvals)
 	if err != nil {
 		return err
 	}
 	severity, location := getLevelFromArgs(kvs)
 	if location >= 0 {
-		append(kvs[:location], kvs[location+2:]...)
+		kvs = append(kvs[:location], kvs[location+2:]...)
 	}
 	l.Logger.Log(severity, kvs...)
+	return nil
 }
 
 func getLevelFromArgs(kvs []interface{}) (logrus.Level, int) {
