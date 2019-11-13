cbf8b7d85db776177d3c0c976a22428d230ace36
Steve Coffman <gears@umich.edu>
2019-11-13 13:43:20 -0500

    master: Add test package names for lint
    Signed-off-by: Steve Coffman <gears@umich.edu>

--- test/logger.go
+++ test/logger.go
@@ -2,10 +2,12 @@ package test
 
 import "github.com/sirupsen/logrus"
 
+// LogWrapper is for testing StackSkip. See stackskip_test.go for details
 type LogWrapper struct {
 	Logger *logrus.Logger
 }
 
+// Error is for testing StackSkip. See stackskip_test.go for details
 func (l *LogWrapper) Error(msg string) {
 	l.Logger.WithField("trace", "1").Error(msg)
 }
